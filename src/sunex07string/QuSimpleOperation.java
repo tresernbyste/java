ceee839446afcfbcc758db399a5d2f714b31479c
DESKTOP-LSO174O\javastudy <tresernbyste@gmail.com>
2023-11-13 18:20:45 +0900

    일일과제

--- src/sunex07string/QuSimpleOperation.java
+++ src/sunex07string/QuSimpleOperation.java
@@ -1,11 +1,33 @@
 package sunex07string;
 
+import java.util.Scanner;
+
 public class QuSimpleOperation
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+//		두개의 정수를 입력받아서 사칙연산의 결과를 출력하는 메소드와 main메소드를 작성해보자. 단 나눗셈은 몫과 나머지를 각각 출력해야 한다.
+//		arithmetic()
+//		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("정수를 입력하세요1");
+		int a = scan.nextInt();
+		System.out.println("정수를 입력하세요2");
+		int b = scan.nextInt();
+
+		arithMetic(a, b);
+	}
+
+	static void arithMetic(int sNum, int eNum)
+	{
+		int s, m, n, mo = 0;
+		s = sNum + eNum;
+		m = sNum - eNum;
+		n = sNum / eNum;
+		mo = sNum % eNum;
+
+		System.out.printf("두수의 사칙연산 결과 +:%2d -:%2d /:%d %s", s, m, n, mo);
 
 	}
 
